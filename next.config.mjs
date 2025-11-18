a8a9b345112aa8583a037639cd6451cc406c841c
dyeyo <dyegovallejob@gmail.com>
2025-11-18 11:47:53 -0500

    Estilos versel

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "placehold.co", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "i.ibb.co", pathname: "/**" },
+      { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
+    ],
+  },
+};
+
+export default nextConfig;
